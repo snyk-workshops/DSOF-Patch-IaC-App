e7e6ab1e69e8928bdfc01d329a905590d8b7b765
cooplad <147500432+cooplad@users.noreply.github.com>
2023-10-10 12:00:39 +0100

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -3,4 +3,5 @@ resource "aws_cloudtrail" "insecure-logging" {
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
   enable_log_file_validation = true
+  is_multi_region_trail = true
 }
