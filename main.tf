ad8ae0cf37063a7abe9ba5ea5ec0f15c88fa2324
cooplad <147500432+cooplad@users.noreply.github.com>
2023-10-10 11:57:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ provider "aws" {
 
 resource "aws_iam_account_password_policy" "strict" {
   minimum_password_length = 14
-  #require_lowercase_characters   = true
-  #require_numbers                = true
-  #require_uppercase_characters   = true
-  #require_symbols                = true
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
   #allow_users_to_change_password = true
   #password_reuse_prevention      = 24 
   max_password_age = 3
