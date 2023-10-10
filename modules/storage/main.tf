8c567cd8434b6cb5909be505a4cd50dfd56c51a5
cooplad <147500432+cooplad@users.noreply.github.com>
2023-10-10 11:40:56 +0100

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "insecure-bucket" {
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 20
-  encrypted         = false
+  encrypted         = true
   tags = {
     Name = "insecure"
   }
