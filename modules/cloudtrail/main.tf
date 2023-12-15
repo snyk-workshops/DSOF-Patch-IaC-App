205ed3c2e7a548a65ff4ae0c93512582ebada9f3
akashsgupta <akashsgupta96@gmail.com>
2023-12-15 21:10:59 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -2,4 +2,6 @@ resource "aws_cloudtrail" "insecure-logging" {
   name           = "cloudtrail-logging"
   s3_bucket_name = "my-cloudtrail-bucket"
   enable_logging = true
+  enable_log_file_validation = true
+  is_multi_region_trail = true
 }
