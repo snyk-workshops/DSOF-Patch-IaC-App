0936bf64c4f42f9b3c03f8def293eee21abacab6
harkirat2910 <152063299+harkirat2910@users.noreply.github.com>
2023-12-19 12:12:06 +0530

    Update main.tf

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "insecure-bucket" {
   versioning.enabled = true
   logging 
   versioning.mfa_delete = true
-  versioning_configuration.mfa_delete = Enabled
 }
 
 # resource "aws_s3_bucket_public_access_block" "insecure-bucket" {
