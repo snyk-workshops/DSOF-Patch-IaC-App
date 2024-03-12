694a215b4e923577df0645cc9c602365523a600e
lucamignani <163158911+lucamignani@users.noreply.github.com>
2024-03-12 16:52:00 +0100

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.16.0.0/24"]
   }
 }
 
